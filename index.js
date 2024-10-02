78016781faef4878ae6641f78dc9320f5723b7b3
Joshua Young <youngjoshua248@icloud.com>
2024-10-02 15:20:10 -0400

    Updated pairedword

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ const multiplesOfThree = () => {
 }
 
 multiplesOfThree();
+
+
+const multiplesOfThreeOrFive = () => { // initializing the arrow function
+    for(let i = 3; i <= 100; i++){ //set the mutliple value of 3 and also telling it not exceed 100
+        if (i % 3 === 0 || i % 5 === 0) { //if statement because we are looking for multiples of 3 or 5
+            console.log(i) // console logging i because we have value set to it(3)
+        }
+    }
+}
+
+multiplesOfThreeOrFive();// calling the the variable
+
+
+//  let untilNum(num) {
+    if (let i = 1; i )
