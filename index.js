ccd2f7d3880b2c78e655b54b6a7d98634c127837
Nando <kommandernando@outlook.com>
2024-10-02 14:21:45 -0400

    Completed multiplesOfThree function

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-//Code your solutions in this file
+// //Code your solutions in this file
 
-const fiveToOneHundred = () =>{
-    for(let i = 5; i <= 100; i++) {
-        console.log(i)
+// const fiveToOneHundred = () =>{
+//     for(let i = 5; i <= 100; i++) {
+//         console.log(i)
+//     }
+// }
+
+// fiveToOneHundred();
+
+const multiplesOfThree = () => {
+    for(let i = 3; i <= 100; i++){
+        if (i % 3 === 0) {
+            i + 3
+            console.log(i)
+        }
     }
 }
 
-fiveToOneHundred();
+multiplesOfThree();
