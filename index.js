8bd18338ae7d736a3ebcd1dcee68761d68bc369d
Joshua Young <youngjoshua248@icloud.com>
2024-10-03 16:39:08 -0400

    completed the assignments

--- index.js
+++ index.js
@@ -68,3 +68,26 @@ const isNegative = (number) => {
 }
 console.log(isNegative(3));
 
+
+
+
+
+
+const betweenTwentyAndFourty = (num) => {
+    return num >= 20 && num <= 40 ;
+}
+
+console.log(betweenTwentyAndFourty(21));
+
+
+
+const largest = (num1, num2, num3) => {
+    if ( num1 > num2 && num1 > num3 ) {
+        return num1;
+    } else if (num2 > num3){
+        return num2;
+    } else {
+        return num3;}
+    }
+
+    console.log(largest(4, 6, 8))
